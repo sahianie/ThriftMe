c2acd4ec03b7414f41210ac43e93996bb200f56f
sahianie <sahianie763@gamil.com>
2025-02-16 23:54:24 -0800

    first_commit

---
+++ resources/views/Front/Partial/Header.blade.php
@@ -0,0 +1,25 @@
+<header class="header-section">
+		<div class="container-fluid">
+			<!-- logo -->
+			<div class="site-logo">
+				<img src="Front/img/logo.png" alt="logo">
+			</div>
+			<!-- responsive -->
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-right">
+				<a href="cart.html" class="card-bag"><img src="img/icons/bag.png" alt=""><span>2</span></a>
+				<a href="#" class="search"><img src="img/icons/search.png" alt=""></a>
+			</div>
+			<!-- site menu -->
+			<ul class="main-menu">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="#">Woman</a></li>
+				<li><a href="#">Man</a></li>
+				<li><a href="#">LookBook</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+	</header>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Front\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('index');
